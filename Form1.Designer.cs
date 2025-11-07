ef9ce3ccac196400613399a5b7fb373d16ed64d2
CodedByMahsa <mahsa2005Zare@gmail.com>
2025-11-07 05:09:01 -1100

    FirstCommit

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Function
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TxtText = new TextBox();
+            TxtChar = new TextBox();
+            BtnWordsList = new Button();
+            BtnNumOfChars = new Button();
+            BtnNumOfWords = new Button();
+            SuspendLayout();
+            // 
+            // TxtText
+            // 
+            TxtText.Location = new Point(59, 62);
+            TxtText.Multiline = true;
+            TxtText.Name = "TxtText";
+            TxtText.Size = new Size(378, 50);
+            TxtText.TabIndex = 0;
+            TxtText.KeyPress += TxtText_KeyPress;
+            // 
+            // TxtChar
+            // 
+            TxtChar.Location = new Point(59, 150);
+            TxtChar.Multiline = true;
+            TxtChar.Name = "TxtChar";
+            TxtChar.Size = new Size(378, 50);
+            TxtChar.TabIndex = 0;
+            TxtChar.KeyPress += TxtChar_KeyPress;
+            // 
+            // BtnWordsList
+            // 
+            BtnWordsList.Location = new Point(59, 242);
+            BtnWordsList.Name = "BtnWordsList";
+            BtnWordsList.Size = new Size(109, 46);
+            BtnWordsList.TabIndex = 1;
+            BtnWordsList.Text = "لیست کلمات";
+            BtnWordsList.UseVisualStyleBackColor = true;
+            BtnWordsList.Click += BtnWordsList_Click;
+            // 
+            // BtnNumOfChars
+            // 
+            BtnNumOfChars.Location = new Point(194, 242);
+            BtnNumOfChars.Name = "BtnNumOfChars";
+            BtnNumOfChars.Size = new Size(109, 46);
+            BtnNumOfChars.TabIndex = 1;
+            BtnNumOfChars.Text = "تعداد حرف";
+            BtnNumOfChars.UseVisualStyleBackColor = true;
+            BtnNumOfChars.Click += BtnNumOfChars_Click;
+            // 
+            // BtnNumOfWords
+            // 
+            BtnNumOfWords.Location = new Point(328, 242);
+            BtnNumOfWords.Name = "BtnNumOfWords";
+            BtnNumOfWords.Size = new Size(109, 46);
+            BtnNumOfWords.TabIndex = 1;
+            BtnNumOfWords.Text = "تعداد کلمه";
+            BtnNumOfWords.UseVisualStyleBackColor = true;
+            BtnNumOfWords.Click += BtnNumOfWords_Click;
+            // 
+            // FrmMain
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(496, 346);
+            Controls.Add(BtnNumOfWords);
+            Controls.Add(BtnNumOfChars);
+            Controls.Add(BtnWordsList);
+            Controls.Add(TxtChar);
+            Controls.Add(TxtText);
+            Name = "FrmMain";
+            Text = "FrmMain";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox TxtText;
+        private TextBox TxtChar;
+        private Button BtnWordsList;
+        private Button BtnNumOfChars;
+        private Button BtnNumOfWords;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+namespace Function
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        string myChar = "";
+        int words = 0;
+        private void TxtText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ' ')
+            {
+                MessageBox.Show("ONLY TEXT IS VALID!");
+                e.Handled = true;
+            }
+        }
+
+        private void TxtChar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && TxtChar.Text.Length >= 1)
+            {
+                MessageBox.Show("ONLY ONE CHARACHTER IS ALLOWED!");
+                e.Handled = true;
+            }
+        }
+
+        private void BtnWordsList_Click(object sender, EventArgs e)
+        {
+            if (TxtChar.Text == null)
+                MessageBox.Show("Fill All TextBoxes please");
+            else
+            {
+                myChar = TxtChar.Text;
+                string[] myText = TxtText.Text.Split(' ');
+                string showWords = "";
+                for (int i = 0; i <myText.Length; i++) 
+                {
+                    if (myText[i].Contains(myChar)) 
+                    {
+                        showWords += myText[i];
+                        showWords += ", ";
+                    }
+                }
+                MessageBox.Show($" List of words : {showWords}");
+            }
+        }
+
+        private void BtnNumOfChars_Click(object sender, EventArgs e)
+        {
+            int repeated = 0;
+            if (TxtChar.Text == null)
+                MessageBox.Show("Fill All TextBoxes please");
+            else
+            {
+                myChar= TxtChar.Text;
+                for (int i = 0; i < TxtText.Text.Length; i++)
+                {
+                    if (TxtText.Text[i].ToString() == myChar) 
+                        repeated++;
+                }
+                MessageBox.Show($"Number Of repeated word : {repeated}");
+            }
+
+        }
+
+        private void BtnNumOfWords_Click(object sender, EventArgs e)
+        {
+            if (TxtChar.Text == null)
+                MessageBox.Show("Fill All TextBoxes please");
+            else
+            {
+                myChar = TxtChar.Text;
+                string[] myText = TxtText.Text.Split(' ');
+                int numWords = 0;
+                for (int i = 0; i < myText.Length; i++)
+                {
+                    if (myText[i].Contains(myChar))
+                    
+                        numWords++;
+                  
+                }
+                MessageBox.Show($"Number Of Words : {numWords}");
+            }
+        }
+     
+    }
+}
